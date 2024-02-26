079c1f647707e1f1b1e9f1f1d36ed7fe7408030c
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-02-26 18:01:50 +0330

    Week1Exercise2_Print_FName_and_LName_and_Phone_and_ID_in_OneLine
    تمرین 2 :برنامه ای بنویسید نام؛ نام خانوادگی؛ شماره همراه و کد
    ملی شما را در خروجی در خطوط پشت سرهم نمایش دهد.

---
+++ Week1/W1E2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1E2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FirstName: {0}, LastName: {1}, PhoneNumber: {2}, IDNumber: {3}", "Amin", "Ghanbarirad", "+981234567890", "1234567890");
+            Console.ReadKey();
+        }
+    }
+}
