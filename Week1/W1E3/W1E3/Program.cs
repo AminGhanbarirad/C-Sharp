9334ec0f98890bf02c45b7d900f49ffccca92c22
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-02-27 00:05:21 +0330

    W1E3
    تمرین 3 : چهار برنامه بنویسید که با فرمت زیر جملاتی متفاوت
    را در خروجی ایجاد کند.

---
+++ Week1/W1E3/W1E3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1E3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CountryName: {0}, Capital: {1}, Currency: {2}, CountryPhoneCode: {3}", "Netherlands", "Amsterdam", "EUR", "+31");
+            Console.WriteLine("CountryName: {0}, Capital: {1}, Currency: {2}, CountryPhoneCode: {3}", "Canada", "Ottawa", "CAD", "+1");
+            Console.WriteLine("CountryName: {0}, Capital: {1}, Currency: {2}, CountryPhoneCode: {3}", "United Kingdom", "London", "GBP", "+44");
+            Console.WriteLine("CountryName: {0}, Capital: {1}, Currency: {2}, CountryPhoneCode: {3}", "France", "Paris", "EUR", "+33");
+            Console.ReadKey();
+        }
+    }
+}
