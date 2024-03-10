65f9051a710e15efb1b41d2e2480d33672053f93
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 01:23:49 +0330

    Week3Exercise6_Triangle_Validator
    تمرین ۶ : برنامه ای بنویسید که سه عدد را از ورودی دریافت و مشخص کند آیا این اعداد می تواند تشکیل یک مثلث دهد یا خير؟ براى اينكه3 عدد بتوانند اضلاع يك مثلث باشند روابط روبرو باید برای انها برقرار باشد.

--- Week3/Week3Exercise6_Triangle_Validator/Program.cs
+++ Week3/Week3Exercise6_Triangle_Validator/Program.cs
@@ -10,6 +10,35 @@ namespace Week3Exercise6_Triangle_Validator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the First Side Length:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Side Length:");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Third Side Length:");
+            double c = double.Parse(Console.ReadLine());
+
+            bool isValid;
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                isValid = true;
+            }
+            else
+            {
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("Triangle can be made with these sides.");
+            }
+            else
+            {
+                Console.WriteLine("Triangle can't be made with these sides");
+            }
+            Console.ReadKey();
         }
     }
 }
