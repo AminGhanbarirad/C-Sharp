97bc5254b664fb779a8d8f61c57a78e4a4ef5a09
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 03:10:34 +0330

    Week4Exercise2_Floyd_Binary_Rectangle
    تمرین ٢: بر نامه ای بنویسید مثلث فلوید که به صورت زیر است را با دریافت عدد ١ به عنوان تعداد سطر ها چاپ کند.

---
+++ Week4/Week4Exercise2_Floyd_Binary_Rectangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise2_Floyd_Binary_Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number of Lines:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
