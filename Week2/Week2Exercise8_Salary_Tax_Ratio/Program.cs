77c02ed59e508c4326433ce6019234bc0041a9cb
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 19:18:29 +0330

    Week2Exercise8_Salary_Tax_Ratio
    تمرین 8 : فرض کنید نحوه محاسبه مالیات حقوق یک کارمند بصورت زير باشد.
    الف) اگر حقوق كمتر از ۵٠٠٠٠٠ باشد مالیاتی تعلق نگیرد.
    ب) اكَر حقوق بين ٥٠٠٠٠٠ و١٠٠٠٠٠٠ باشد ١٠ درصد مالیات تعلق می گیرد.
    ج) اگر حقوق بیشتر از ١٠٠٠٠٠٠ باشد ١۵ درصد مالیات تعلق می گیرد.
    برنامه ای بنویسید که حقوق کارمند را دریافت و میزان مالیات حقوق را محاسبه و چاپ کند

---
+++ Week2/Week2Exercise8_Salary_Tax_Ratio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise8_Salary_Tax_Ratio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Salary:");
+            int salary = int.Parse(Console.ReadLine());
+
+            int tax;
+            if (salary < 500000)
+            {
+                tax = 0;
+            }
+            else if (salary < 1000000)
+            {
+                tax = salary * 10 / 100;
+            }
+            else
+            {
+                tax = salary * 15 / 100;
+            }
+
+            Console.WriteLine("Amount of Tax Related to Salary: {0}", tax);
+
+            Console.ReadKey();
+        }
+    }
+}
