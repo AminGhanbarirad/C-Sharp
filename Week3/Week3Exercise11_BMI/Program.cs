6a95e2e912ff99f895743b48c6603f7e115f77ff
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 01:47:11 +0330

    Week3Exercise11_BMI
    تمرین ا ١: ابتدا وزن و قد خود را محاسبه کنید سپس برنامه ای بنویسید که این دو عدد را به عنوان ورودی دریافت کند سپس بر اساس فرمول شاخص BMI که تقسیم وزن به مربع قد(متر) است و بر اساس جدول زیر خروجى مناسب را به همراه شاخص BMI نمایش دهد.

---
+++ Week3/Week3Exercise11_BMI/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Exercise11_BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Height In Centimeter");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Weight in Kilogram");
+            int weight = int.Parse(Console.ReadLine());
+
+            double heightInM = height / 100;
+
+            double BMI = weight / Math.Pow(heightInM, 2);
+
+            Console.WriteLine(BMI);
+            Console.ReadKey();
+
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("You're Underweighted");
+                Console.ReadKey();
+                return;
+            }
+            else if (18.5 <= BMI & BMI < 25)
+            {
+                Console.WriteLine("You're Weight is Normal");
+                Console.ReadKey();
+                return;
+            }
+            else if (25 <= BMI & BMI < 30)
+            {
+                Console.WriteLine("You're Overweighted");
+                Console.ReadKey();
+                return;
+            }
+            else if (BMI > 30)
+            {
+                Console.WriteLine("You're Obese");
+                Console.ReadKey();
+                return;
+            }
+
+        }
+    }
+}
