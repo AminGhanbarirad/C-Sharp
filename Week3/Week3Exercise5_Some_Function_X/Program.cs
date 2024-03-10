7ec0e18fe25c7eb162280fe13df0fca3c7b87256
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 23:34:43 +0330

    Week3Exercise5_Some_Function_X
    تمرین ۵:برنامه ای بنویسید که عدد X را از ورودی دریافت و براساس فرمول زیر مقدار بدست آمدہ را در خروجی چاپ کند

---
+++ Week3/Week3Exercise5_Some_Function_X/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Exercise5_Some_Function_X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number:");
+            double x = double.Parse(Console.ReadLine());
+
+            double result;
+            if (x < 0)
+            {
+                result = x + 1;
+            }
+            else if (1 <= x)
+            {
+                result = 1 / x;
+            }
+            else
+            {
+                result = -Math.Pow(x, 2);
+            }
+
+            Console.WriteLine("Result: {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
