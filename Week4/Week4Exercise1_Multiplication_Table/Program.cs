4c4f9318b87c953d7a21e45f3fc0ec30c2ac8f9b
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 02:56:21 +0330

    Week4Exercise1_Multiplication_Table
    تمرین1 : برنامه اي بنویسید که جدول ضرب را در خروجی نمایش دهد.

---
+++ Week4/Week4Exercise1_Multiplication_Table/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise1_Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("   | ");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("{0,2} ", i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("{0,2} | ", i);
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write("{0,2} ", i * j);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
