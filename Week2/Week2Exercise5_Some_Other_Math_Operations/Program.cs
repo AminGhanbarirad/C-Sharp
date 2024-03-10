938fce66c42ee668d26e7c3c00df9586bd685918
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 16:32:43 +0330

    Week2Exercise5_Some_Other_Math_Operations
    تمرین5 : برنامه ای بنویسید که دو عدد x و y را از ورودی دریافت و عملیات زیر را انجام و نتیجه را در خروجی چاپ نماید.
    
    (x+y)*z
    
    x*y+y*z

---
+++ Week2/Week2Exercise5_Some_Other_Math_Operations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise5_Some_Other_Math_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter x:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter y:");
+            int y = int.Parse(Console.ReadLine());
+
+            int summation = (x + y);
+
+            Console.WriteLine("(x + y) * z: {0} {1}", summation, "z");
+            Console.WriteLine("x * y + y * z: {0} {1} {2} {3}", summation, "+", y, "z");
+
+            Console.ReadKey();
+        }
+    }
+}
