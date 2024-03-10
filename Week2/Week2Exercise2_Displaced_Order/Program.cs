ad188cf154bff8336c4e130fd9be5b6299d6b2dc
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 10:34:27 +0330

    Week2Exercise2_Displaced_Order
    برنامه ای بنویسید که دو عدد را از ورودی دریافت و آن دو را جابه جا و در خروجی چاپ کند.

---
+++ Week2/Week2Exercise2_Displaced_Order/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise2_Displaced_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First Number:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double temp = num1;
+            num1 = num2;
+            num2 = temp;
+
+            Console.WriteLine("The First Number After Reorder: " + num1);
+            Console.WriteLine("The Second Number After Reorder: " + num2);
+
+            Console.ReadKey();
+        }
+    }
+}
