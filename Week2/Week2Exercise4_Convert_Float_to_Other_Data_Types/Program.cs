a8f029f9098313622a7d102ec2f0d4ff81ec5d6c
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 16:17:46 +0330

    Week2Exercise4_Convert_Float_to_Other_Data_Types
    تمرين 4: برنامه اى بنويسيد كه عددى اعشارى از نوع float را از ورودی دریافت و با تبدیل آن به نوع داده های زير؛ آنها رادر خروجی چاپ نماید.
    
    Int
    Short
    Byte
    Bool
    Long
    Char
    
    String رو خودم اضافه کردم.

---
+++ Week2/Week2Exercise4_Convert_Float_to_Other_Data_Types/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise4_Convert_Float_to_Other_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Decimal Number:");
+            float number = float.Parse(Console.ReadLine());
+
+            int intNumber = (int)number;
+
+            short shortNumber = (short)number;
+
+            byte byteNumber = (byte)number;
+
+            bool boolNumber = number != 0;
+
+            long longNumber = (long)number;
+
+            char charNumber = (char)number;
+
+            string strNumber = number.ToString();
+
+
+            Console.WriteLine("int: " + intNumber);
+            Console.WriteLine("short: " + shortNumber);
+            Console.WriteLine("byte: " + byteNumber);
+            Console.WriteLine("bool: " + boolNumber);
+            Console.WriteLine("long: " + longNumber);
+            Console.WriteLine("char: " + charNumber);
+            Console.WriteLine("string: " + strNumber);
+
+
+            Console.ReadKey();
+        }
+    }
+}
