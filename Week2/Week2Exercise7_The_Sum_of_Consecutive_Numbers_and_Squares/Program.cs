cf2dcbd618c9a0b54e2c1f62161b87501993ac98
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 19:05:51 +0330

    Week2Exercise7_The_Sum_of_Consecutive_Numbers_and_Squares
    تمرین 7 : بر نامه ای بنویسید کە عددی را از ورودی دریافت و نتیجه عملیات زیر را در خروجی چاپ نماید. توجه کنید از حلقه تکرار استفاده نشود.
    
    1+2+3+ ... +n
    1^2+2^2+3^2+ ... +n^2

---
+++ Week2/Week2Exercise7_The_Sum_of_Consecutive_Numbers_and_Squares/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise7_The_Sum_of_Consecutive_Numbers_and_Squares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = (n * (n + 1)) / 2;
+
+            int squares = (n * (n + 1) * (2 * n + 1)) / 6;
+
+            Console.WriteLine("Consecutive Summation 1 to {0}: {1}", n, sum);
+            Console.WriteLine("Square Summation 1 to {0}: {1}", n, squares);
+
+            Console.ReadKey();
+        }
+    }
+}
