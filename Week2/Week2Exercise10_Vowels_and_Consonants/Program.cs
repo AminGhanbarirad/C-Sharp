bcd0e1ffcb630a14390598dfab254c0ae7bdb33c
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 20:05:37 +0330

    Week2Exercise10_Vowels_and_Consonants
    تمرین10 : برنامه ای بنویسید کە کاراکتری را از ورودی دريافت و با استفاده از Switch case مشخص كند حرف صدادار است يا خير؟

---
+++ Week2/Week2Exercise10_Vowels_and_Consonants/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise10_Vowels_and_Consonants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Character:");
+            char ch = Console.ReadLine()[0];
+
+            switch (ch)
+            {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("'{0}' is Vowel", ch);
+                    break;
+                default:
+                    Console.WriteLine("'{0}' is Consonant", ch);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
