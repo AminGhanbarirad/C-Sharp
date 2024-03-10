2a95fbdd281983169041d7aedad3d2d563e574cc
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 22:25:18 +0330

    Week2Exercise3_Grade_Converter
    تمرین ٣ : بر نامه ای بنویسید که نمره یک درس دانشجو را از ورودی خواندہ(از صفر تا ١٠٠) سپس معادل حرفی آن را بر اساس جدول زیر در خروجی چاپ نماید.

---
+++ Week3/Week2Exercise3_Grade_Converter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise3_Grade_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Grade Score:");
+            int score = int.Parse(Console.ReadLine());
+
+            string grade;
+            if (score < 0 || score > 100)
+            {
+                Console.WriteLine("Invalid Score!");
+                Console.ReadKey();
+                return;
+            }
+            if (score >= 80)
+            {
+                grade = "A";
+            }
+            else if (score >= 61)
+            {
+                grade = "B";
+            }
+            else if (score >= 41)
+            {
+                grade = "C";
+            }
+            else if (score >= 21)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "E";
+            }
+
+            Console.WriteLine("Score Conversion: {0}", grade);
+            Console.ReadKey();
+        }
+    }
+}
