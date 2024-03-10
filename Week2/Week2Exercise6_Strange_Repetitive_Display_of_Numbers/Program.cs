bf4554e7b7d2550168a8b961c2aa795d613270f4
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 17:03:37 +0330

    Week2Exercise6_Strange_Repetitive_Display_of_Numbers
    تمرین ۶:بر نامه ای بنویسید کە عددی را از ورودی دریافت و در
    ۵ سطر و ٣ ستون عدد را بصورت نمونه چاپ نماید:
    
    5-5-5
    -5-5-5-
    555
    5 !- 5&*5
    555

---
+++ Week2/Week2Exercise6_Strange_Repetitive_Display_of_Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise6_Strange_Repetitive_Display_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            double number = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("{0}-{0}-{0}", number);
+
+            //option1
+            Console.WriteLine("-{0}-{0}-{0}-", number);
+            
+            //option2 - useless         (┬┬﹏┬┬)
+            //Console.Write("-");
+            //for (double i = 0; i < 4; i++)
+            //{
+            //    Console.Write("{0}-", number);
+            //}
+            //Console.WriteLine();
+
+
+            Console.WriteLine("{0}{0}{0}", number);
+
+
+            Console.WriteLine("{0}!-{1}&*5", number, number);
+
+
+            Console.WriteLine("{0} {0} {0}", number);
+
+
+            Console.ReadKey();
+        }
+    }
+}
