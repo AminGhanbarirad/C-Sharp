9d2de79507f7faacd9b5fb26f128405c9a0f06b2
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 15:58:18 +0330

    Week2Exercise3_Temperature_Conversion
    تمرین ٣:بر نامه ای بنویسید که عددی را به عنوان درجه سانتیگراد از ورودی دریافت و آن را به کلوین و فارنهایت تبدیل و در خروجی چاپ کند.

---
+++ Week2/Week2Exercise3_Temperature_Conversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Exercise3_Temperature_Conversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Temperature in Celsius Degree:");
+            double celsius = double.Parse(Console.ReadLine());
+
+            double kelvin = celsius + 273.15;
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("Kelvin: " + kelvin);
+            Console.WriteLine("Fahrenheit Degree: " + fahrenheit);
+
+            Console.ReadKey();
+        }
+    }
+}
