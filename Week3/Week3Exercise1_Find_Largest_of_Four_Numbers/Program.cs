cdc050da08c470e4fc735b29b878748f27906234
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 21:35:50 +0330

    Week3Exercise1_Find_Largest_of_Four_Numbers
    تمرین ١: بر نامه ای بنویسید که ۴ عدد را از ورودی خوانده سپس بزر گترین عدد میان این ۴ عدد را در خروجی چاپ نماید.

---
+++ Week3/Week3Exercise1_Find_Largest_of_Four_Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Exercise1_Find_Largest_of_Four_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the First Number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Number:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Third Number:");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Fourth Number:");
+            int num4 = int.Parse(Console.ReadLine());
+
+            int max = num1;
+            if (num2 > max)
+            {
+                max = num2;
+            }
+            if (num3 > max)
+            {
+                max = num3;
+            }
+            if (num4 > max)
+            {
+                max = num4;
+            }
+
+            Console.WriteLine("The Biggest Number: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
