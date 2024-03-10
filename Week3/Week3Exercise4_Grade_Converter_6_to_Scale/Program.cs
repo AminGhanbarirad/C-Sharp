a7e15d8555f61d4f7499f3f0825b57a539a02a7f
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-10 22:51:02 +0330

    Week3Exercise4_Grade_Converter_6_to_Scale
    تمرین ۴: در برنامه تمرین شماره با استفادە از switch / case معادل حرفی نمره دانشجو که از ۶ تا ١٠ می باشد را در خروجى نمایش دهيد

---
+++ Week3/Week3Exercise4_Grade_Converter_6_to_Scale/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Exercise4_Grade_Converter_6_to_Scale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Grade Score:");
+            double score = double.Parse(Console.ReadLine());
+
+            string grade;
+            if (score < 6 || score > 10)
+            {
+                Console.WriteLine("Invalid Score!");
+                Console.ReadKey();
+                return;
+            }
+            if (score == 10)
+            {
+                grade = "A";
+            }
+            else if (score >= 9)
+            {
+                grade = "B";
+            }
+            else if (score >= 8)
+            {
+                grade = "C";
+            }
+            else if (score >= 7)
+            {
+                grade = "D";
+            }
+            else
+            {
+                grade = "E";
+            }
+
+            Console.WriteLine("Score Conversion: {0}", grade);
+            Console.ReadKey();
+        }
+    }
+}
