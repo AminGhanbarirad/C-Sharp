227ffb75bb328d073b290853fb70000cd9c36f7b
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 22:31:37 +0330

    Week4Exercise5_fourDigitsDividableOn7Not3
    تمرين 5 : بر نامه ای بنویسید که تمام اعداد چهاررقمی که بر ٧بخشپذیرند اما بر ٣بخشپذير نيستند را زير هم نمايش دهد.

---
+++ Week4/Week4Exercise5_fourDigitsDividableOn7Not3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise5_fourDigitsDividableOn7Not3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i < 10000; i++)
+            {
+                if (i % 7 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
