c9d425e3d2791f94ba2677c80335bdc57e9e5d2f
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 23:12:43 +0330

    Week4Exercise6_Divisor
    تمرین ۶ : برنامه ای بنویسید که عدد صحیحی را گرفته و همه ی مقسوم عليه هايش را به همراه تعداد آن ها در خروجى نمایش دهد.

---
+++ Week4/Week4Exercise6_Divisor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise6_Divisor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int j = 0;
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (x % i == 0) 
+                {
+                    Console.Write("| {0} |", i);
+                    j++;
+                }
+            }
+            Console.WriteLine("\nTotal of Divisor: {0}", j);
+            Console.ReadKey();
+        }
+    }
+}
