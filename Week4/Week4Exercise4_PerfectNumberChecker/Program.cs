2412c233138aea64005222b6138e7f2eb516570a
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-11 22:15:07 +0330

    Week4Exercise4_PerfectNumberChecker
    تمرین ۴:برنامه ای بنویسید کە عددی را از ورودی دریافت و مشخص كند عدد كامل است یا خير؟

---
+++ Week4/Week4Exercise4_PerfectNumberChecker/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise4_PerfectNumberChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Nuumber:");
+            int x = int.Parse(Console.ReadLine());
+
+            bool perfect = false;
+            
+            int j = 1;
+            
+            for (int i = 2 ; i < x ; i++)
+            {
+                if ( x % i == 0)
+                {
+                    j += i;
+                }
+            }
+
+            if ( x == j )
+            {
+                perfect = true;
+            }
+            
+            if (perfect)
+            {
+                Console.WriteLine("Number {0} Is Perfect.", x);
+            }
+            else
+            {
+                Console.WriteLine("Number {0} Is Not Perfect.", x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
