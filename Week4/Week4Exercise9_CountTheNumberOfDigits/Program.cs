67248fde074ff5a6fc0873d792ff0d05de2584ab
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-13 10:52:52 +0330

    Week4Exercise9_CountTheNumberOfDigits
    تمرین ٩: برنامه ای بنویسید که عدد صحیحی را کرفته و تعداد رقمهايش را نمايش دهد.

---
+++ Week4/Week4Exercise9_CountTheNumberOfDigits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise9_CountTheNumberOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter An Integer Number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int j = 0;
+
+            for (int i = 1; x/i != 0; i*=10)
+            {
+                j++;
+            }
+            Console.WriteLine("The Number Of Digits is: {0}", j);
+            Console.ReadKey();
+        }
+    }
+}
