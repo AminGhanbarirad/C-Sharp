7e566d770027a6f3003ffc2ade8512ec31f059ef
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-13 16:55:06 +0330

    Week4Exercise15_ProductOfNumberAndItsReverse
    تمرین ١۵: برنامە ای بنویسید کە عددی طبیعی با تعداد رقم نامشخص را گرفته، مقلوب آن را به دست آورده و حاصلضرب آن عدد در مقلوبش را محاسبه و نمایش دهد، مثلا ا گر کاربر عدد ٢٣١۵ را وارد کند چیزی که روی مونیتور بايد ديده شود نتيجه حاصلضرب دو عدد ٢٣١۵و ٥١٣٢باشد.

---
+++ Week4/Week4Exercise15_ProductOfNumberAndItsReverse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise15_ProductOfNumberAndItsReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int l = CountTheNumberOfDigits(x);
+            Console.WriteLine("Length of number: {0}", l);
+
+            double product = 0;
+            int temp = x;
+
+            for (int i = 1; i <= l; i++)
+            {
+                product += x * (temp % 10) * Math.Pow(10, (l - i));
+                temp /= 10;
+            }
+            Console.WriteLine("Product of number and its reverse is: {0}", product);
+            Console.ReadKey();
+        }
+
+        static int CountTheNumberOfDigits(int x)
+        {
+            int l = 0;
+
+            for (int i = 1; x / i != 0; i *= 10)
+            {
+                l++;
+            }
+            return l;
+        }
+    }
+}
