de48fef60870fec8129a6bd55f59ae0116385116
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-13 15:58:34 +0330

    Week4Exercise14_HarshadOrNivenNumber2Digits
    تمرین ۴ا: بر نامه ای بنویسید که تمام اعداد دورقمی را که بر مجموع ارقامشان بخشپذیر ند را زیر هم نمایش دهد.

---
+++ Week4/Week4Exercise14_HarshadOrNivenNumber2Digits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise14_HarshadOrNivenNumber2Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            for (int i = 10; i < 100; i++)
+            {
+                s = (i % 10) + (i / 10);
+                if (i % s == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
