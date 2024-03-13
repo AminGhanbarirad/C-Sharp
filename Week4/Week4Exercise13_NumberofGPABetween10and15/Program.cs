e8b674251f84e8887dbfec074446d64e8742443d
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-13 15:42:06 +0330

    Week4Exercise13_NumberofGPABetween10and15
    تمرین ١٣: بر نامه ای بنویسید که ابتدا تعداد دانشجویان یک کلاس، سپس معدلھای تک تک آنها را گرفته و درنهایت تعداد دانشجویانی که معدلشان بین ده و پانزده است را مشخص کند

---
+++ Week4/Week4Exercise13_NumberofGPABetween10and15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise13_NumberofGPABetween10and15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of students: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("Enter the student number{0} GPA: ", i + 1);
+                int y = int.Parse(Console.ReadLine());
+                Console.Write("");
+                
+                if (y >= 10 && y < 15)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("There is/are {0} student/s whit GPA between 10 and 15.", count);
+            Console.ReadKey();
+        }
+    }
+}
