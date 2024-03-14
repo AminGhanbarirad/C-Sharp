bf14539aa7792de257a0360353da0f7a8ddf270c
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-14 17:23:16 +0330

    Week4Exercise18_FourDigitPrimeNumbers
    تمرین ١٨: برنامه ای بنویسید که همه اعداد اول چهاررقمی را نمایش دهد

---
+++ Week4/Week4Exercise18_FourDigitPrimeNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise18_FourDigitPrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i < 10000; i++)
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.Write("{0} ,", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
