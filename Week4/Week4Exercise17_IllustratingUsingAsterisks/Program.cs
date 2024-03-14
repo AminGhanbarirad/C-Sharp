1bca079db04ca72c1bcfb82b00bc8a0b02f58ae7
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-14 17:01:50 +0330

    Week4Exercise17_IllustratingUsingAsterisks
    تمرین ١٧: برای ترسیم هر کدام از شکلهای زیر با کاراکتر ستاره، برنامه ای بنویسید.

---
+++ Week4/Week4Exercise17_IllustratingUsingAsterisks/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise17_IllustratingUsingAsterisks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//////////////////////////////////////////////////
+            for ( int j = 14;  j > 0; j -= 1 )
+            { 
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < j - 1; i++)
+                {
+                    Console.Write(" ");
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+//////////////////////////////////////////////////
+            for (int j = 14; j > 0; j -= 1)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write("  ");
+                }
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.Write(" ");
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write("  ");
+                }
+                for (int i = 0; i < 6; i++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+//////////////////////////////////////////////////
+            for (int j = 1; j < 14; j++)
+            {
+                for (int i = 0; i < 40; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < 40; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < j; i++)
+                {
+                    Console.Write(" ");
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            //////////////////////////////////////////////////
+            int k = 1;
+            while (k < 16)
+            {
+                for (int i = 0; i < 16 - k; i++)
+                {
+                    Console.Write(" ");
+                }
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write("* ");
+                }
+                k++;
+                Console.WriteLine();
+            }
+//////////////////////////////////////////////////
+            Console.ReadKey();
+        }
+    }
+}
