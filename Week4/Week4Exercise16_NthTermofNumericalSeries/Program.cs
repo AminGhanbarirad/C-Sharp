6e09f8178bdb49fefd05306572ee9f049c0da12f
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-14 13:38:24 +0330

    Week4Exercise16_NthTermofNumericalSeries
    تمرین 16 : برنامه اى بنويسيد كه عدد صحيحى مثل nرا كرفته و جمله ی ام سری زیر را روی مونیتور نمایش دهد، در سری زیر از جمله ی سوم به بعد هر جمله میانگین دو جمله ی قبل است:
    1,5,3,4,3.5,3.75

---
+++ Week4/Week4Exercise16_NthTermofNumericalSeries/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Exercise16_NthTermofNumericalSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the N number:");
+            int n = int.Parse(Console.ReadLine());
+
+            double prev1 = 1;
+            double prev2 = 5;
+            Console.Write("{0}, {1}, ", prev1, prev2);
+            
+            double avg = 0;
+            
+            for (int i = 3; i <= n; i++)
+            {
+                avg = (prev1 + prev2) / 2;
+                Console.Write("{0} ,", avg);
+                prev1 = prev2;
+                prev2 = avg;
+            }
+            Console.ReadKey();
+        }
+    }
+}
