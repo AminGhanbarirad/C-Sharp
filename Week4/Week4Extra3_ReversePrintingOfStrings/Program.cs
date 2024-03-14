b9a698bdc36f36f5044d7464d05d14ba03583853
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-03-15 00:38:55 +0330

    Week4Extra3_ReversePrintingOfStrings
    برنامه ای بنویسید که رشته ای را از ورودی دریافت و بصورت عکس در خروجی چاپ نماید.

---
+++ Week4/Week4Extra3_ReversePrintingOfStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Extra3_ReversePrintingOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String: ");
+            string str = Console.ReadLine();
+
+            string reversedStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversedStr += str[i];
+            }
+
+            Console.WriteLine("Reverse order Of the strings: " + reversedStr);
+            Console.ReadKey();
+        }
+    }
+}
