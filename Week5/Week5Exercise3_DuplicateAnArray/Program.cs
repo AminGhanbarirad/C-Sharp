dcb71860f8afe26846ab92e0db2147d5e6b3864e
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-04-02 13:28:56 +0330

    Week5Exercise3_DuplicateAnArray
    برنامه ای بنوسید که یک آرایه را در آرایه دیگر کپی کند.

---
+++ Week5/Week5Exercise3_DuplicateAnArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Exercise3_DuplicateAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[20];
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Numbers[i] = i + 1;
+            }
+            
+            Console.WriteLine("The Orginal Array: ");
+            foreach (int n in Numbers)
+            {
+                Console.Write(n + ", ");
+            }
+            Console.WriteLine("");
+
+            
+            int[] NewNumbers = new int[Numbers.Length];
+
+            int j = 0;
+
+            for (int i = 0; i < NewNumbers.Length; i++)
+            {
+                NewNumbers[j] = Numbers[i];
+                j++;
+            }
+
+            Console.WriteLine("Copy of The Orginal Array: ");
+            foreach (int n in NewNumbers)
+            {
+                Console.Write(n + ", ");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
