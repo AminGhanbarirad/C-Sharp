f28f59f0b9d64b220aba7b9ea6a2c3c6362e9e0d
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-04-02 12:21:03 +0330

    Week5Exercise1_DoesNumberExistInArray
    برنامه ای بنویسید که مشخص كند یک عدد خاص در آرايه وجود دارد يا خير؟ در صورت وجود اندیس آن را چاپ کند.

--- Week5/Week5Exercise1_DoesNumberExistInArray/Program.cs
+++ Week5/Week5Exercise1_DoesNumberExistInArray/Program.cs
@@ -10,6 +10,29 @@ namespace Week5Exercise1_DoesNumberExistInArray
     {
         static void Main(string[] args)
         {
+            int[] PrimeNmbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+            
+            Console.WriteLine("Enter a number from 1 to 100 to check if it is Prime or Not! :");
+            int n = int.Parse(Console.ReadLine());
+
+            bool isPrime = false;
+
+            foreach (int i in PrimeNmbers)
+            {
+                if (n == i)
+                {
+                    isPrime = true;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("{0} is Prime! ", n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is Not Prime! ", n);
+            }
+            Console.ReadKey();
         }
     }
 }
