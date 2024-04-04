a56124b018fc2e73c42220129533b2766a631be9
AminGhanbarirad <ghanbarirad.amin@gmail.com>
2024-04-05 03:01:58 +0330

    Week5Exercise10_ShiftAllArrayElementsCounterClockWise
    برنام های بنویسید که یک آرایه را در خلاف جهت عقربه های ساعت شیفت دهد.

---
+++ Week5/Week5Exercise10_ShiftAllArrayElementsCounterClockWise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5Exercise10_ShiftAllArrayElementsCounterClockWise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            int res = search(array, 5);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+        static int search(int[] array, int num)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
