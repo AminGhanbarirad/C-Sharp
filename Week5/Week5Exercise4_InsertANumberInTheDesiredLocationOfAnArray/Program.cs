109d72d0a020c75e5b8dd8460592ed8e3cc1241c
AminGhanbarirad <107618535+AminGhanbarirad@users.noreply.github.com>
2024-04-30 10:47:51 +0330

    Update Program.cs

--- Week5/Week5Exercise4_InsertANumberInTheDesiredLocationOfAnArray/Program.cs
+++ Week5/Week5Exercise4_InsertANumberInTheDesiredLocationOfAnArray/Program.cs
@@ -12,26 +12,21 @@ namespace Week5Exercise4_InsertANumberInTheDesiredLocationOfAnArray
         {
             int[] numbers = new int[5] { 1, 3, 5, 7, 9 };
 
-            // دریافت عدد و مکان مورد نظر از کاربر
             Console.Write("Num: ");
             int num = int.Parse(Console.ReadLine());
             Console.Write("Place: ");
             int index = int.Parse(Console.ReadLine());
 
-            // درج عدد در آرایه
             if (index >= 0 && index < numbers.Length)
             {
-                // جابجایی عناصر آرایه
                 for (int i = numbers.Length - 1; i >= index; i--)
                 {
                     numbers[i + 1] = numbers[i];
                 }
 
-                // درج عدد در مکان مورد نظر
                 numbers[index] = num;
 
-                // چاپ آرایه جدید
-                Console.WriteLine("آرایه جدید:");
+                Console.WriteLine("New Array:");
                 foreach (int n in numbers)
                 {
                     Console.Write(n + " ");
@@ -39,7 +34,7 @@ namespace Week5Exercise4_InsertANumberInTheDesiredLocationOfAnArray
             }
             else
             {
-                Console.WriteLine("مکان مورد نظر نامعتبر است!");
+                Console.WriteLine("index is invalid!");
             }
 
             Console.ReadKey();
